6a9145196d918bcf9a81be6239b8b745d44a9e87
JaeHyeong Yoon <yoonwogud@gmail.com>
2026-04-24 16:29:21 +0900

    Time: 0 ms (100%), Space: 20 MB (48.27%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,19 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        # O(n) binary search
+        start = 0
+        end = len(nums) -1
+        mid = 0
+        while start <= end:
+            mid = (start + end) // 2 
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                start = mid +1
+            else:
+                end = mid -1
+        return start
+
+
+
