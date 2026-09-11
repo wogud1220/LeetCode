a3af4e78aa39ff49a5603eea224c3b22c3613f47
JaeHyeong Yoon <yoonwogud@gmail.com>
2026-09-11 21:37:34 +0900

    Time: 33 ms (6.19%), Space: 19.3 MB (48.85%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+
+        answer = 0
+
+        for i in range(1,n+1):
+            if (i % 3) == 0 or (i%5 == 0) or (i%7 == 0):
+                answer += i
+
+        return answer
